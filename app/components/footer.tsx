390f6c33a2bc5ba542deca413c25282936504b93
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-10-18 00:17:34 +0200

    Add hover link styling

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -12,25 +12,25 @@ export default function Footer() {
           <div className="order-1 grid grid-cols-2 text-gray-600 sm:order-3">
             <ul className="space-y-4">
               <li className="font-medium text-gray-900">Site</li>
-              <li>
+              <li className="hover:text-fair_dark_blue-600">
                 <Link href={"#"}>About</Link>
               </li>
-              <li>
+              <li className="hover:text-fair_dark_blue-600">
                 <Link href={"#"}>Contact</Link>
               </li>
-              <li>
+              <li className="hover:text-fair_dark_blue-600">
                 <Link href={"#"}>Privacy</Link>
               </li>
             </ul>
             <ul className="space-y-4">
               <li className="font-medium text-gray-900">Resources</li>
-              <li>
+              <li className="hover:text-fair_dark_blue-600">
                 <Link href={"#"}>Documentation</Link>
               </li>
-              <li>
+              <li className="hover:text-fair_dark_blue-600">
                 <Link href={"#"}>Glossary</Link>
               </li>
-              <li>
+              <li className="hover:text-fair_dark_blue-600">
                 <Link href={"#"}>Source Code</Link>
               </li>
             </ul>
@@ -84,7 +84,7 @@ export default function Footer() {
         </div>
         <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
           <div className="flex space-x-6 md:order-2">
-            <Link href={""} className="text-gray-400 hover:text-gray-500">
+            <Link href={""} className="text-gray-400 hover:text-fair_dark_blue-600">
               <span className="sr-only">LinkedIn</span>
               <svg
                 stroke="currentColor"
@@ -100,7 +100,7 @@ export default function Footer() {
                 <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"></path>
               </svg>
             </Link>
-            <Link href={""} className="text-gray-400 hover:text-gray-500">
+            <Link href={""} className="text-gray-400 hover:text-fair_dark_blue-600">
               <span className="sr-only">X</span>
               <svg
                 stroke="currentColor"
@@ -116,7 +116,7 @@ export default function Footer() {
                 <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path>
               </svg>
             </Link>
-            <Link href={""} className="text-gray-400 hover:text-gray-500">
+            <Link href={""} className="text-gray-400 hover:text-fair_dark_blue-600">
               <span className="sr-only">WatchApp</span>
               <svg
                 stroke="currentColor"
