f1dbf1cbf4c2f8d16bb72db986830a995e335f0f
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-10-17 14:58:25 +0200

    Setup Tailwind class sorting and integrated it with eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-blue-500 text-2xl">Hello FAIR-Aware</h1>
+      <h1 className="text-2xl text-blue-500">Hello FAIR-Aware</h1>
     </div>
   );
 }
