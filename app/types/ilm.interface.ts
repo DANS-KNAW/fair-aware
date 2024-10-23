c48311d7d81d671982025b49c451d6d6a73eac21
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-10-23 16:30:55 +0200

    Add ILM type

---
+++ app/types/ilm.interface.ts
@@ -0,0 +1,49 @@
+export interface ILM {
+  header: ILMHeader;
+  footer: ILMFooter;
+  landingPage: ILMLandingpage;
+  assessment: any;
+  about: any;
+  contact: any;
+  admin: any;
+}
+
+interface ILMHeader {
+  about: string;
+  documentation: string;
+  contact: string;
+}
+
+interface ILMFooter {
+  description: {
+    link: string;
+    text: string;
+  };
+  navigation: {
+    site: {
+      header: string;
+      about: string;
+      contact: string;
+      privacy: string;
+    };
+    resources: {
+      header: string;
+      documentation: string;
+      glossary: string;
+      sourceCode: string;
+    };
+  };
+  copyright: string;
+}
+
+interface ILMLandingpage {
+  title: string;
+  introduction: string; // Look into chopping up the different paragraph's
+  form: {
+    header: string;
+    group: string;
+    language: string;
+    dot: string;
+    button: string;
+  };
+}
