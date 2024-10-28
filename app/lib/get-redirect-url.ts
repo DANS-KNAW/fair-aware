0e54ac4e8db9aea54c4da91acef9c4023dc2d810
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-10-28 07:38:58 +0100

    Add function to handle local tunnel development

---
+++ app/lib/get-redirect-url.ts
@@ -0,0 +1,26 @@
+import { headers } from "next/headers";
+
+/**
+ * Method to handle local development.
+ *
+ * The application needs a reverse tunnel or valid HTTPS route
+ * for SSO callbacks. The isssue is that Response takes the server
+ * origin not the tunnels.
+ *
+ * This function makes sure to use the tunnels URL when in
+ * development mode.
+ * 
+ * Note: Since the application uses ***ONLY*** SSO we expect
+ * to always have an HTTPS URL.
+ *
+ * @param path route to redirect to.
+ * @returns url with or without origin
+ */
+export default function getRedirectUrl(path: string): string {
+  if (process.env.NODE_ENV === "development") {
+    const headersList = headers();
+    const domain = headersList.get("host") || "";
+    return "https://" + domain + path;
+  }
+  return path;
+}
