29eda1561a6dbeb86a9f60e9ce25fa3877f9f428
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-09 11:28:54 +0100

    Add basic database environment varables namespace

--- api-fair-impact/src/app.module.ts
+++ api-fair-impact/src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import commonConfig from './config/common.config';
+import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.local', '.env.development', '.env.production'],
-      load: [commonConfig],
+      load: [commonConfig, databaseConfig],
     }),
   ],
   controllers: [AppController],
