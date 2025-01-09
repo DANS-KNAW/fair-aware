9fb7045d92ca44f2d8ab572a76aabb4aa2791b89
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-09 20:36:23 +0100

    Setup TypeORM Async module

--- api-fair-impact/src/app.module.ts
+++ api-fair-impact/src/app.module.ts
@@ -4,13 +4,15 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import commonConfig from './config/common.config';
 import databaseConfig from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.env.local', '.env.development', '.env.production'],
+      envFilePath: ['../.env'],
       load: [commonConfig, databaseConfig],
     }),
+    TypeOrmModule.forRootAsync(databaseConfig.asProvider()),
   ],
   controllers: [AppController],
   providers: [AppService],
