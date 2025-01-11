9cc3ca6e60a9b4097d7d2dabd1aa47f007df808d
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-11 17:10:05 +0100

    Initial DigitalObjectType entity

--- api-fair-impact/src/digital-object-types/entities/digital-object-type.entity.ts
+++ api-fair-impact/src/digital-object-types/entities/digital-object-type.entity.ts
@@ -1,1 +1,34 @@
-export class DigitalObjectType {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+/**
+ * Digital Object Type (DOT) represents things like datasets, software, etc.
+ */
+@Entity()
+export class DigitalObjectType {
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  /**
+   * The full name of the Digital Object Type.
+   */
+  @Column()
+  label: string;
+
+  /**
+   * A 4 digit short code that can be used to identify the Digital Object Type.
+   */
+  @Column()
+  code: string;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+}
