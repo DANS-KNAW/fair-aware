1a1002e012b07cfe9fbdfdf4c82d9c5e115bb165
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-12 16:16:13 +0100

    Inject DOT entity as repository

--- api-fair-impact/src/digital-object-types/digital-object-types.module.ts
+++ api-fair-impact/src/digital-object-types/digital-object-types.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DigitalObjectTypesService } from './digital-object-types.service';
 import { DigitalObjectTypesController } from './digital-object-types.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DigitalObjectType } from './entities/digital-object-type.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DigitalObjectType])],
   controllers: [DigitalObjectTypesController],
   providers: [DigitalObjectTypesService],
 })
