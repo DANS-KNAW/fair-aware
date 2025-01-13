d675f1a33c5dc2e6fde151891a87ffba4c5bcc7b
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-13 06:39:37 +0100

    Linted IsGlobalAlpha decorator

--- api-fair-impact/src/decorators/is-global-alpha.ts
+++ api-fair-impact/src/decorators/is-global-alpha.ts
@@ -1,8 +1,4 @@
-import {
-  registerDecorator,
-  ValidationOptions,
-  ValidationArguments,
-} from 'class-validator';
+import { registerDecorator, ValidationOptions } from 'class-validator';
 
 /**
  * Custom decorator to validate that a string contains only global alphabetic characters
@@ -11,18 +7,18 @@ import {
  * @TODO Should still be tested with a variety of languages to ensure it works as expected.
  */
 export function IsGlobalAlpha(validationOptions?: ValidationOptions) {
-  return function (object: Object, propertyName: string) {
+  return function (object: object, propertyName: string) {
     registerDecorator({
       name: 'isGlobalAlpha',
       target: object.constructor,
       propertyName: propertyName,
       options: validationOptions,
       validator: {
-        validate(value: any, args: ValidationArguments) {
+        validate(value: any) {
           const regex = /^[\p{L}\p{M}\s]+$/u;
           return typeof value === 'string' && regex.test(value); // Ensure it's a string and matches the regex
         },
-        defaultMessage(args: ValidationArguments) {
+        defaultMessage() {
           return 'The value contains invalid characters. Only letters from languages around the world are allowed.';
         },
       },
