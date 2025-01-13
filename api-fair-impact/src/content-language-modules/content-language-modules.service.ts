6c30be5a9b95db634e30546d8c0d31f39e96449f
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-13 13:35:10 +0100

    Add create method CLM

--- api-fair-impact/src/content-language-modules/content-language-modules.service.ts
+++ api-fair-impact/src/content-language-modules/content-language-modules.service.ts
@@ -1,11 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { CreateContentLanguageModuleDto } from './dto/create-content-language-module.dto';
 import { UpdateContentLanguageModuleDto } from './dto/update-content-language-module.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ContentLanguageModule } from './entities/content-language-module.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ContentLanguageModulesService {
-  create(createContentLanguageModuleDto: CreateContentLanguageModuleDto) {
-    return 'This action adds a new contentLanguageModule';
+  private readonly logger = new Logger(ContentLanguageModulesService.name, {
+    timestamp: true,
+  });
+
+  constructor(
+    @InjectRepository(ContentLanguageModule)
+    private readonly contentLanguageModuleRepository: Repository<ContentLanguageModule>,
+  ) {}
+
+  async create(
+    createContentLanguageModuleDto: CreateContentLanguageModuleDto,
+  ): Promise<ContentLanguageModule> {
+    let contentLanguageModule = this.contentLanguageModuleRepository.create(
+      createContentLanguageModuleDto,
+    );
+
+    try {
+      contentLanguageModule = await this.contentLanguageModuleRepository.save(
+        contentLanguageModule,
+      );
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Content Language Module already exists!');
+      }
+      this.logger.error(error);
+      throw new InternalServerErrorException(
+        'Failed to create contentLanguageModule!',
+      );
+    }
+
+    return contentLanguageModule;
   }
 
   findAll() {
