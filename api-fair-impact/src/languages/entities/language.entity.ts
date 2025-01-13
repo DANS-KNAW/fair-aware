b264090e26c3b718f4261fc07c2abee0f4dce4c8
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-13 08:37:58 +0100

    Add initial Language entity

--- api-fair-impact/src/languages/entities/language.entity.ts
+++ api-fair-impact/src/languages/entities/language.entity.ts
@@ -1,1 +1,51 @@
-export class Language {}
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { IsGlobalAlpha } from 'src/decorators/is-global-alpha';
+import {
+  Entity,
+  UpdateDateColumn,
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryColumn,
+  Column,
+} from 'typeorm';
+
+@Entity()
+export class Language {
+  /**
+   * The language code following ISO 639-1 standard.
+   */
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(2)
+  @PrimaryColumn()
+  code: string;
+
+  /**
+   * The full english name of the language.
+   */
+  @IsNotEmpty()
+  @IsString()
+  @IsGlobalAlpha()
+  @MaxLength(255)
+  @Column()
+  englishLabel: string;
+
+  /**
+   * The full name of the language in the language itself.
+   */
+  @IsNotEmpty()
+  @IsString()
+  @IsGlobalAlpha()
+  @MaxLength(255)
+  @Column()
+  nativeLabel: string;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @DeleteDateColumn({ type: 'timestamptz' })
+  deletedAt: Date;
+}
