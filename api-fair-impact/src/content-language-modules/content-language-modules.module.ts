09e51f6820bae47894dd19124fe65f5210890e1c
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-13 06:40:54 +0100

    Loaded CLM entity in TypeORM

--- api-fair-impact/src/content-language-modules/content-language-modules.module.ts
+++ api-fair-impact/src/content-language-modules/content-language-modules.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ContentLanguageModulesService } from './content-language-modules.service';
 import { ContentLanguageModulesController } from './content-language-modules.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ContentLanguageModule } from './entities/content-language-module.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ContentLanguageModule])],
   controllers: [ContentLanguageModulesController],
   providers: [ContentLanguageModulesService],
 })
