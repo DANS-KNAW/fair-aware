940b7484700fc32046e6d8a1668305fd88c22cfd
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-14 18:21:53 +0100

    Update config namespace and set API port with env

--- api-fair-impact/src/config/common.config.ts
+++ api-fair-impact/src/config/common.config.ts
@@ -1,5 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('common', () => ({
+export const COMMON_CONFIG_KEY = 'common';
+
+export default registerAs(COMMON_CONFIG_KEY, () => ({
   apiPort: parseInt(process.env.API_PORT, 10) || 3000,
 }));
--- api-fair-impact/src/main.ts
+++ api-fair-impact/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService, ConfigType } from '@nestjs/config';
+import commonConfig, { COMMON_CONFIG_KEY } from './config/common.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const commonConfig =
+    configService.get<ConfigType<typeof commonConfig>>(COMMON_CONFIG_KEY);
+
+  await app.listen(commonConfig.apiPort ?? 3000);
 }
 bootstrap();
