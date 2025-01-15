709494a907136c155eb8a89b6efaa46b83d97efb
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-15 21:30:26 +0100

    Add initial asessment entity

--- api-fair-impact/src/assessments/entities/assessment.entity.ts
+++ api-fair-impact/src/assessments/entities/assessment.entity.ts
@@ -1,1 +1,55 @@
-export class Assessment {}
+import {
+  IsJSON,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  Length,
+  MaxLength,
+} from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Assessment {
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID()
+  @PrimaryGeneratedColumn('uuid')
+  uuid: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsJSON()
+  @Column({ type: 'jsonb' })
+  answerSchema: object;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 2)
+  languageCode: string;
+
+  @IsNotEmpty()
+  @IsString()
+  dotSchemaVersion: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(6)
+  @Column()
+  dotCode: string;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @DeleteDateColumn({ type: 'timestamptz' })
+  deletedAt: Date;
+}
