815625bd2ebdce35d660eec04ad90dc3d79deee0
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-15 11:10:59 +0100

    Add prose setup for styling of dynamicly set HTML

--- client-fair-impact/components/assessment/support-drawer-arcordion.tsx
+++ client-fair-impact/components/assessment/support-drawer-arcordion.tsx
@@ -57,7 +57,7 @@ export default function SupportDrawerAccordion({
           </dt>
           <DisclosurePanel as="dd" className="mt-2 pr-12">
             <div
-              className="text-sm text-gray-600"
+              className="prose prose-a:underline prose-a:text-fair_dark_blue-600 hover:prose-a:text-fair_dark_blue-400 space-y-4 text-sm text-gray-600"
               dangerouslySetInnerHTML={{ __html: material.text }}
             />
           </DisclosurePanel>
--- client-fair-impact/tailwind.config.ts
+++ client-fair-impact/tailwind.config.ts
@@ -1,4 +1,5 @@
 import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -65,5 +66,5 @@ export default {
       },
     },
   },
-  plugins: [forms],
+  plugins: [forms, typography],
 } satisfies Config;
