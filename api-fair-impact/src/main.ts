957ec80d2c1b5b8cddd0ec8b67858b131c16948d
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-16 10:14:46 +0100

    Outlined network environemnts

--- api-fair-impact/src/main.ts
+++ api-fair-impact/src/main.ts
@@ -1,18 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService, ConfigType } from '@nestjs/config';
-import commonConfig, { COMMON_CONFIG_KEY } from './config/common.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
 
-  const configService = app.get(ConfigService);
-  const commonConfigValue =
-    configService.get<ConfigType<typeof commonConfig>>(COMMON_CONFIG_KEY);
-
-  await app.listen(commonConfigValue.apiPort ?? 3000);
+  await app.listen(3001);
 }
 bootstrap();
