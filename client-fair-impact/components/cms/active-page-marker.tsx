8e659198982f581a7a8b780f56ed29cf53079c78
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-18 22:20:55 +0100

    Add component to mark active link

---
+++ client-fair-impact/components/cms/active-page-marker.tsx
@@ -0,0 +1,29 @@
+import { remToPx } from "@/lib/remToPx";
+import { CMSNavigationGroup } from "@/types/cms-navigation-group.interface";
+import { motion } from "framer-motion";
+
+export default function ActivePageMarker({
+  group,
+  pathname,
+}: {
+  group: CMSNavigationGroup;
+  pathname: string;
+}) {
+  const itemHeight = remToPx(2);
+  const offset = remToPx(0.2);
+  const activePageIndex = group.links.findIndex(
+    (link) => link.href === pathname,
+  );
+  const top = offset + activePageIndex * itemHeight;
+
+  return (
+    <motion.div
+      layout
+      className="absolute left-2 h-6 w-[2px] rounded-xl bg-fair_light_blue-400"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 0.2 } }}
+      exit={{ opacity: 0 }}
+      style={{ top }}
+    />
+  );
+}
