b3b28d901e7fc3c5c7bbaca4c6c1bdbfe855878e
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-18 19:40:58 +0100

    Add OAuthButton component

---
+++ client-fair-impact/components/oauth-button.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+
+interface OAuthButtonProps {
+  /**
+   * Icon for the OAuth sign-on method.
+   */
+  icon: string;
+  /**
+   * Label displayed next to the icon.
+   */
+  label: string;
+  /**
+   * Callback function that is called when the button is clicked.
+   */
+  callback: () => void;
+  /**
+   * Whether the button is disabled.
+   */
+  disabled?: boolean;
+}
+
+/**
+ * Button for OAuth sign-on methods.
+ */
+export function OAuthButton({
+  icon,
+  label,
+  callback,
+  disabled,
+}: OAuthButtonProps) {
+  return (
+    <div
+      className={`flex w-full items-center justify-center rounded-md border border-gray-300 p-2 ${
+        disabled
+          ? "cursor-not-allowed opacity-50"
+          : "cursor-pointer hover:border-fair_dark_blue-200 hover:bg-gray-900/5"
+      }`}
+      onClick={!disabled ? callback : undefined}
+    >
+      <div className="relative h-5 w-auto">
+        <Image
+          src={icon}
+          alt=""
+          width={0}
+          height={0}
+          sizes="100vh"
+          style={{ height: "100%", width: "auto" }}
+        />
+      </div>
+      <span className="ml-3 mt-0.5 text-sm text-gray-800">{label}</span>
+    </div>
+  );
+}
