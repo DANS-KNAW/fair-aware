f055af49940a8e1fd1e62de04526af0ed0ae6a7c
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-20 09:43:34 +0100

    Add function to convert timestampz to readable date

---
+++ client-fair-impact/lib/timestampz-to-date.tsx
@@ -0,0 +1,16 @@
+/**
+ * Converts a timestamp string to a formatted date string.
+ *
+ * @param {string} timestampz - The timestamp string to convert.
+ * @param {boolean} [time=false] - Whether to include the time in the formatted string.
+ * @returns {string} The formatted date string. If `time` is true, the formatted string includes the time.
+ */
+export function TimestampzToDate(
+  timestampz: string,
+  time: boolean = false,
+): string {
+  const date = new Date(timestampz);
+  const formattedDate = date.toLocaleDateString("en-GB");
+  const formattedTime = date.toLocaleTimeString("en-GB", { hour12: false });
+  return time ? `${formattedDate}, ${formattedTime}` : formattedDate;
+}
