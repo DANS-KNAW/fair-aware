6bc262807bb55279aeaf263864d7aed3b26420f7
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-20 14:14:49 +0100

    Updated create method for relation validation

--- api-fair-impact/src/assessments/assessments.service.ts
+++ api-fair-impact/src/assessments/assessments.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Injectable,
   InternalServerErrorException,
   Logger,
@@ -9,6 +10,9 @@ import { UpdateAssessmentDto } from './dto/update-assessment.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Assessment } from './entities/assessment.entity';
 import { Repository } from 'typeorm';
+import { LanguagesService } from 'src/languages/languages.service';
+import { DigitalObjectTypesService } from 'src/digital-object-types/digital-object-types.service';
+import { DigitalObjectTypeSchemasService } from 'src/digital-object-type-schemas/digital-object-type-schemas.service';
 
 @Injectable()
 export class AssessmentsService {
@@ -19,23 +23,48 @@ export class AssessmentsService {
   constructor(
     @InjectRepository(Assessment)
     private readonly assessmentRepository: Repository<Assessment>,
+    private readonly languagesService: LanguagesService,
+    private readonly digitalObjectTypesService: DigitalObjectTypesService,
+    private readonly digitalObjectTypeSchemasService: DigitalObjectTypeSchemasService,
   ) {}
 
   async create(createAssessmentDto: CreateAssessmentDto): Promise<Assessment> {
     /**
      * @TODO There should be logic here that validates the given schema compare to the specified DOT schema.
      */
-
-    let assessment = this.assessmentRepository.create(createAssessmentDto);
-
     try {
+      // find language with lanuge code
+      const language = await this.languagesService.findOne(
+        createAssessmentDto.languageCode,
+      );
+
+      if (!language) {
+        throw new BadRequestException('Invalid language code!');
+      }
+
+      const digitalObjectType = await this.digitalObjectTypesService.findOne(
+        createAssessmentDto.dotCode,
+      );
+
+      if (!digitalObjectType) {
+        throw new BadRequestException('Invalid DOT code!');
+      }
+
+      let assessment = this.assessmentRepository.create({
+        ...createAssessmentDto,
+      });
+
       assessment = await this.assessmentRepository.save(assessment);
+
+      return assessment;
     } catch (error) {
+      if (error instanceof BadRequestException) {
+        throw error;
+      }
+
       this.logger.error(error);
       throw new InternalServerErrorException('Failed to create assessment!');
     }
-
-    return assessment;
   }
 
   async findAll(page: number = 1, amount: number = 50): Promise<Assessment[]> {
