bcd0af4c9639dfa588d869c11a54dee06a96f821
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-20 16:08:30 +0100

    Style changes

--- client-fair-impact/app/cms/layout.tsx
+++ client-fair-impact/app/cms/layout.tsx
@@ -8,7 +8,7 @@ export default function CMSLayout({
       <Header />
       <div className="relative flex h-full flex-col px-4 pt-14 sm:px-6 lg:px-8">
         <main className="flex-auto">
-          <div className="[html_:where(&>*)]:mx-auto [html_:where(&>*)]:max-w-40rem] [html_:where(&>*)]:lg:mx-[calc(50%-min(50%,33rem))] [html_:where(&>*)]:2xl:max-w-[66rem]">
+          <div className="[html_:where(&>*)]:max-w-40rem] [html_:where(&>*)]:mx-auto [html_:where(&>*)]:lg:mx-[calc(50%-min(50%,33rem))] [html_:where(&>*)]:2xl:max-w-[66rem]">
             {children}
           </div>
         </main>
