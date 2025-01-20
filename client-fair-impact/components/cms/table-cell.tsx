b5523985ac0740f796ec7040b889baf1e3d896c4
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-20 09:36:34 +0100

    Add colSpan prop to TableCell component

--- client-fair-impact/components/cms/table-cell.tsx
+++ client-fair-impact/components/cms/table-cell.tsx
@@ -1,9 +1,18 @@
 interface TableCellProps {
+  colSpan?: number;
   children: React.ReactNode | React.ReactNode[];
 }
 
-export default function TableCell({ children }: Readonly<TableCellProps>) {
+export default function TableCell({
+  colSpan,
+  children,
+}: Readonly<TableCellProps>) {
   return (
-    <td className="whitespace-nowrap py-4 text-sm text-gray-500">{children}</td>
+    <td
+      colSpan={colSpan}
+      className="whitespace-nowrap py-4 text-sm text-gray-500"
+    >
+      {children}
+    </td>
   );
 }
