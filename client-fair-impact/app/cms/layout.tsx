31b198a4d28f2ce4364b99bcf58f34ad66a61972
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-20 06:14:17 +0100

    Add spacing class for screen sizes

--- client-fair-impact/app/cms/layout.tsx
+++ client-fair-impact/app/cms/layout.tsx
@@ -7,7 +7,11 @@ export default function CMSLayout({
     <div className="h-full lg:ml-72 xl:ml-80">
       <Header />
       <div className="relative flex h-full flex-col px-4 pt-14 sm:px-6 lg:px-8">
-        <main className="flex-auto">{children}</main>
+        <main className="flex-auto">
+          <div className="[html_:where(&>*)]:mx-auto [html_:where(&>*)]:max-w-2xl [html_:where(&>*)]:lg:mx-[calc(50%-min(50%,theme(maxWidth.lg)))] [html_:where(&>*)]:lg:max-w-3xl">
+            {children}
+          </div>
+        </main>
       </div>
     </div>
   );
