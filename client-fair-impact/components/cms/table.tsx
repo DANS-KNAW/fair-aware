d26a677541fd2971d0f657ff8e22bba8b3e81b6b
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-20 08:56:28 +0100

    Add table, body, header component

---
+++ client-fair-impact/components/cms/table.tsx
@@ -0,0 +1,37 @@
+import TableHeaderCell from "./table-header-cell";
+
+interface TableProps {
+  children: React.ReactNode | React.ReactNode[];
+}
+
+export function TableBody({
+  children,
+}: Readonly<{ children: React.ReactNode | React.ReactNode[] }>) {
+  return <tbody className="divide-y divide-gray-200">{children}</tbody>;
+}
+
+export function TableHeader({
+  children,
+}: Readonly<{ children: React.ReactNode | React.ReactNode[] }>) {
+  return (
+    <thead>
+      <tr>{children}</tr>
+    </thead>
+  );
+}
+
+export default function Table({
+  children,
+}: Readonly<{ children: React.ReactNode | React.ReactNode[] }>) {
+  return (
+    <div className="mt-8 flow-root">
+      <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <table className="min-w-full divide-y divide-gray-300">
+            {children}
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
