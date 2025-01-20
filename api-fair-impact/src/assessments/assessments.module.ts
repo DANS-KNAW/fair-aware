6f3c3ec38a872764a78b680c93480888aa647ed2
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-20 14:10:36 +0100

    Imported service providers form language dot and dot schema

--- api-fair-impact/src/assessments/assessments.module.ts
+++ api-fair-impact/src/assessments/assessments.module.ts
@@ -3,9 +3,17 @@ import { AssessmentsService } from './assessments.service';
 import { AssessmentsController } from './assessments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Assessment } from './entities/assessment.entity';
+import { LanguagesModule } from 'src/languages/languages.module';
+import { DigitalObjectTypesModule } from 'src/digital-object-types/digital-object-types.module';
+import { DigitalObjectTypeSchemasModule } from 'src/digital-object-type-schemas/digital-object-type-schemas.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Assessment])],
+  imports: [
+    TypeOrmModule.forFeature([Assessment]),
+    LanguagesModule,
+    DigitalObjectTypesModule,
+    DigitalObjectTypeSchemasModule,
+  ],
   controllers: [AssessmentsController],
   providers: [AssessmentsService],
 })
