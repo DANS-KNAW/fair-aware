3286dcc335e96ff8b70ed593b83fea849b5a72c7
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-20 14:09:53 +0100

    Add CreateAssessmentDto class

--- api-fair-impact/src/assessments/dto/create-assessment.dto.ts
+++ api-fair-impact/src/assessments/dto/create-assessment.dto.ts
@@ -1,1 +1,9 @@
-export class CreateAssessmentDto {}
+import { PickType } from '@nestjs/mapped-types';
+import { Assessment } from '../entities/assessment.entity';
+
+export class CreateAssessmentDto extends PickType(Assessment, [
+  'answerSchema',
+  'dotCode',
+  'dotSchemaVersion',
+  'languageCode',
+]) {}
