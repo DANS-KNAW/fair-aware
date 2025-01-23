593374d6a80ee6ca391fde20b2c54428da2759a8
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-23 17:13:33 +0100

    Add active status and active version status to DOT overview

--- client-fair-impact/types/digital-object-type.interface.ts
+++ client-fair-impact/types/digital-object-type.interface.ts
@@ -2,6 +2,12 @@ export interface DigitalObjectType {
   uuid: string;
   label: string;
   code: string;
+
+  digitalObjectTypeSchemas: {
+    version: string;
+    active: boolean;
+  }[];
+
   updatedAt: string;
   createdAt: string;
   deletedAt: string;
