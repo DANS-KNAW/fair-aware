ebd27195c2539e873f583e691ce3eb85a4ea03fb
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-26 21:25:51 +0100

    Add heading section DOT overview

--- client-fair-impact/app/cms/digital-object-types/page.tsx
+++ client-fair-impact/app/cms/digital-object-types/page.tsx
@@ -5,6 +5,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
+import Link from "next/link";
 
 export default async function DOTPage() {
   const queryClient = new QueryClient();
@@ -14,8 +15,26 @@ export default async function DOTPage() {
   });
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <CMSDOTTable />
-    </HydrationBoundary>
+    <>
+      <div className="flex flex-col items-center justify-between sm:flex-row">
+        <div>
+          <h1 className="mb-2 text-2xl font-bold text-gray-800">
+            Digital Object Types
+          </h1>
+          <p className="text-base text-gray-600">
+            Browse and manage Digital Object Types (DOTs) or create a new one.
+          </p>
+        </div>
+        <Link
+          href="/cms/digital-object-types/new"
+          className="bg-fair_dark_blue-600 hover:bg-fair_dark_blue-500 focus-visible:outline-fair_dark_blue-600 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2"
+        >
+          Create DOT
+        </Link>
+      </div>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <CMSDOTTable />
+      </HydrationBoundary>
+    </>
   );
 }
