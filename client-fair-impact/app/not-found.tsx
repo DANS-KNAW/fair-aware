38cee213d599be8bee663a29def3bf2e01120611
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-26 20:42:34 +0100

    Add not found page

---
+++ client-fair-impact/app/not-found.tsx
@@ -0,0 +1,21 @@
+import Footer from "@/components/footer/footer";
+import Header from "@/components/header/header";
+import Link from "next/link";
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <Header />
+      <div className="mx-auto my-20 max-w-7xl space-y-4 px-2 py-12 text-center lg:px-8">
+        <h1 className="text-2xl font-bold text-gray-800">Page not found</h1>
+        <p className="text-gray-600">
+          Sorry, we couldn't find the page you're looking for
+        </p>
+        <Link href="/" className="text-fair_dark_blue-600 underline">
+          Go back home
+        </Link>
+      </div>
+      <Footer />
+    </>
+  );
+}
