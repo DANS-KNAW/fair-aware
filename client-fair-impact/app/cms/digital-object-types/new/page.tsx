c6db5ab5200fc3fd7d2c7c7e045f79bd608e59e1
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-26 14:41:38 +0100

    extracted form section for SSR

--- client-fair-impact/app/cms/digital-object-types/new/page.tsx
+++ client-fair-impact/app/cms/digital-object-types/new/page.tsx
@@ -1,3 +1,5 @@
+import CreateDOTForm from "@/components/form/form-sections/create-dot-form";
+
 export default function NewDOTPage() {
   return (
     <>
@@ -8,78 +10,7 @@ export default function NewDOTPage() {
         Some placeholder text here. This page will be used to create a new DOT.
       </p>
 
-      <div className="mt-12 border-t border-gray-900/10">
-        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div className="sm:col-span-3">
-            <label
-              htmlFor="first-name"
-              className="block text-sm/6 font-medium text-gray-900"
-            >
-              Label
-            </label>
-            <div className="mt-2">
-              <input
-                id="first-name"
-                name="first-name"
-                type="text"
-                autoComplete="given-name"
-                className="block w-full rounded-md border-gray-300 bg-white px-3 py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              />
-            </div>
-          </div>
-
-          <div className="sm:col-span-3">
-            <label
-              htmlFor="last-name"
-              className="block text-sm/6 font-medium text-gray-900"
-            >
-              Code
-            </label>
-            <div className="mt-2">
-              <input
-                id="last-name"
-                name="last-name"
-                type="text"
-                autoComplete="family-name"
-                className="block w-full rounded-md border-gray-300 bg-white px-3 py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-              />
-            </div>
-          </div>
-
-          <div className="col-span-full">
-            <label
-              htmlFor="about"
-              className="block text-sm/6 font-medium text-gray-900"
-            >
-              Description
-            </label>
-            <div className="mt-2">
-              <textarea
-                id="about"
-                name="about"
-                rows={3}
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                defaultValue={""}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button
-          type="button"
-          className="cursor-pointer text-sm/6 font-semibold text-gray-900"
-        >
-          Cancel
-        </button>
-        <button
-          type="submit"
-          className="bg-fair_dark_blue-600 hover:bg-fair_dark_blue-500 focus-visible:outline-fair_dark_blue-600 cursor-pointer rounded-md px-3 py-2 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2"
-        >
-          Create
-        </button>
-      </div>
+      <CreateDOTForm />
     </>
   );
 }
