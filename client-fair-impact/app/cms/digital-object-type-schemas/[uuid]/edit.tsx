6522660ad2f601f8e8b15c356cefefd99af993f9
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-27 11:55:39 +0100

    Add remove principle in DOTS edit action

--- client-fair-impact/app/cms/digital-object-type-schemas/[uuid]/edit.tsx
+++ client-fair-impact/app/cms/digital-object-type-schemas/[uuid]/edit.tsx
@@ -35,6 +35,12 @@ export default function DOTSEditView({ dots }: DOTSReadViewProps) {
     ]);
   };
 
+  const handleDeletePrinciple = (index: number) => {
+    const newAssessment = [...watchAssessment];
+    newAssessment.splice(index, 1);
+    setValue("assessment", newAssessment);
+  };
+
   const handleAddCriterium = (index: number) => {
     const newAssessment = [...watchAssessment];
     newAssessment[index].criteria.push({
@@ -153,10 +159,32 @@ export default function DOTSEditView({ dots }: DOTSReadViewProps) {
             key={"PRINCIPLE" + principleIndex}
             className={`mt-10 grid grid-cols-1 gap-x-6 gap-y-8 border-t border-gray-400 pt-8 sm:grid-cols-6`}
           >
-            <div className="sm:col-span-full">
+            <div className="flex items-center justify-between sm:col-span-full">
               <h3 className="block text-base/6 font-medium text-gray-900">
                 Principle - {principleIndex + 1}
               </h3>
+              <button
+                type="button"
+                onClick={() => handleDeletePrinciple(principleIndex)}
+                className="hover:text-fair_dark_blue-600 cursor-pointer text-gray-800"
+              >
+                <span className="sr-only">Delete Principle</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  aria-hidden="true"
+                  className="size-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </button>
             </div>
 
             {/* We do not check if the length of criteria is empty as an principle should ALWAYS have one criteria. */}
