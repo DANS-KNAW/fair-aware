265bf19f207c52d3c45ae8f50d23d1d5eb1697aa
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-27 08:46:58 +0100

    Removed DOTS creation button

--- client-fair-impact/app/cms/digital-object-type-schemas/page.tsx
+++ client-fair-impact/app/cms/digital-object-type-schemas/page.tsx
@@ -20,23 +20,13 @@ export default async function SchemaPage() {
   return (
     <>
       <Breadcrumbs />
-      <div className="flex flex-col items-center justify-between sm:flex-row">
-        <div>
-          <h1 className="mb-2 text-2xl font-bold text-gray-800">
-            Digital Object Type Schemas
-          </h1>
-          <p className="text-base text-gray-600">
-            Browse and manage Digital Object Type Schemas (DOTS) or create a new
-            one.{" "}
-          </p>
-        </div>
-        <Link
-          href="/cms/digital-object-type-schemas/new"
-          className="bg-fair_dark_blue-600 hover:bg-fair_dark_blue-500 focus-visible:outline-fair_dark_blue-600 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2"
-        >
-          Create DOTS
-        </Link>
-      </div>
+      <h1 className="mb-2 text-2xl font-bold text-gray-800">
+        Digital Object Type Schemas
+      </h1>
+      <p className="text-base text-gray-600">
+        Browse and manage Digital Object Type Schemas (DOTS) or create a new
+        one.
+      </p>
 
       <HydrationBoundary state={dehydrate(queryClient)}>
         <DOTSClientPage />
