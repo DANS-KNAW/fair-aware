48d0715135212086b61157879d64279e6dc871bd
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-01-27 14:56:51 +0100

    Add abstract class as schema interface

---
+++ api-fair-impact/src/digital-object-type-schemas/schemas/schemas.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export abstract class SchemasService<
+  TBaseSchema = object,
+  TContentSchema = object,
+> {
+  /**
+   * Get the base schema for the specific schema type
+   */
+  abstract getBaseSchema(): TBaseSchema;
+
+  /**
+   * Validate if the given schema matched the expected structure.
+   */
+  abstract validateSchema(schema: TBaseSchema): boolean;
+
+  /**
+   * Compare two schemas and return if they are equal.
+   */
+  abstract compareSchemas(): boolean;
+
+  /**
+   * Get the content schema based on the sturcture of the schema.
+   */
+  abstract getContentSchema(schemaStructure: TBaseSchema): TContentSchema;
+
+  /**
+   * Validate the content schema based on the structure of the schema.
+   */
+  abstract validateContentSchema(schema: TContentSchema): boolean;
+}
