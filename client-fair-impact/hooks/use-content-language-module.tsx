5fc46f6225533799c5b666f92d501a0c42ca7d3a
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-02-17 07:58:31 +0100

    Add useContentLanguageModule hook

---
+++ client-fair-impact/hooks/use-content-language-module.tsx
@@ -0,0 +1,21 @@
+import { IContentLanguageModule } from "@/types/entities/content-language-module.interface";
+import { useQuery } from "@tanstack/react-query";
+
+export const fetchContentLanguageModule = async (
+  uuid: string,
+): Promise<IContentLanguageModule> => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_HOST}/content-language-modules/${uuid}`,
+  );
+  if (!response.ok) {
+    throw new Error("Failed to retrieve content language modules");
+  }
+  return response.json();
+};
+
+export default function useContentLanguageModules(uuid: string) {
+  return useQuery<IContentLanguageModule>({
+    queryKey: ["contentLanguageModules", uuid],
+    queryFn: () => fetchContentLanguageModule(uuid),
+  });
+}
