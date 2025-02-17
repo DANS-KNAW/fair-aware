71dc1ba8aa3d8cf3a62387cb329eaa70aced5f65
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-02-17 07:26:30 +0100

    Updated CLMs type to new return

--- client-fair-impact/types/entities/content-language-module.interface.ts
+++ client-fair-impact/types/entities/content-language-module.interface.ts
@@ -1,15 +1,18 @@
-import { ContentLanguageModuleFairAwareTemplate } from "../assessment-template-fair-aware.interface";
-import { IDigitalObjectType } from "./digital-object-type.interface";
-
 export interface IContentLanguageModule {
   uuid: string;
-  schema: ContentLanguageModuleFairAwareTemplate; // Currently only an single schema is supported but this will change in the future.
-  version: string;
-  active: boolean;
-
-  digitalObjectType: IDigitalObjectType;
-
-  createdAt: Date;
   updatedAt: Date;
-  deletedAt: Date;
+  createdAt: Date;
+  deletedAt: Date | null;
+  digitalObjectType: {
+    uuid: string;
+    label: string;
+    code: string;
+  };
+  digitalObjectTypeSchema: {
+    version: string;
+  };
+  language: {
+    code: string;
+    englishLabel: string;
+  };
 }
