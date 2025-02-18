2c21dc79a31c7fbeeaff00bb400d4dcaa2ac6c85
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-02-18 09:15:03 +0100

    Add logic for getting assessment

--- client-fair-impact/app/assessment/page.tsx
+++ client-fair-impact/app/assessment/page.tsx
@@ -8,12 +8,17 @@ import {
 import AssessmentBuilder from "@/components/assessment/assessment-builder";
 import { fetchContentLanguageModuleByLanguageAndDOT } from "@/hooks/use-content-language-module-by-language-and-dot";
 
-export default async function AssessmentPage() {
+export default async function AssessmentPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ dot: string; lang: string }>;
+}) {
   const queryClient = new QueryClient();
+  const { dot, lang } = await searchParams;
+
   await queryClient.prefetchQuery({
-    queryKey: ["contentLanguageModule", "en", "assessment"],
-    queryFn: () =>
-      fetchContentLanguageModuleByLanguageAndDOT("en", "assessment"),
+    queryKey: ["contentLanguageModule", lang, "assessment"],
+    queryFn: () => fetchContentLanguageModuleByLanguageAndDOT(lang, dot),
   });
 
   return (
@@ -21,7 +26,7 @@ export default async function AssessmentPage() {
       <Header />
       <main className="mx-auto max-w-7xl px-2 lg:px-8">
         <HydrationBoundary state={dehydrate(queryClient)}>
-          <AssessmentBuilder />
+          <AssessmentBuilder lang={lang} dot={dot} />
         </HydrationBoundary>
       </main>
       <Footer />
