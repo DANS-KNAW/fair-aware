e6d818d08617ef01a6b6f4164f826f3c337a1ec2
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-03-15 19:15:25 +0100

    Setup test outline for DOT provider

--- api-fair-impact/src/digital-object-types/digital-object-types.service.spec.ts
+++ api-fair-impact/src/digital-object-types/digital-object-types.service.spec.ts
@@ -15,4 +15,59 @@ describe('DigitalObjectTypesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('create', () => {
+    test.todo('Should create a new DOT successfully');
+    test.todo('Should handle duplicate DOT code');
+    test.todo('Should handle database errors gracefully');
+  });
+
+  describe('findAll', () => {
+    test.todo(
+      'Should return the first 10 DOTs when no query params are provided',
+    );
+    test.todo(
+      'Should return the next 10 DOTs when the page query param is set to 2',
+    );
+    test.todo(
+      'Should return the first 10 DOTs when an invalid page query param is provided',
+    );
+    test.todo('Should return an empty array if no DOTs are available');
+    test.todo(
+      'Should properly handle cases when the database returns an error',
+    );
+  });
+
+  describe('findOne', () => {
+    test.todo('Should return a DOT with the specified UUID if it exists');
+    test.todo('Should return a 404 error if the specified UUID does not exist');
+    test.todo('Should handle database errors gracefully');
+  });
+
+  describe('findOneByCode', () => {
+    test.todo('Should return a DOT with the specified code');
+    test.todo('Should return a 404 error if the specified code does not exist');
+    test.todo('Should handle database errors gracefully');
+  });
+
+  describe('update', () => {
+    test.todo('Should update a DOT successfully');
+    test.todo('Should handle database errors gracefully');
+    test.todo('Should return a 404 error if the specified UUID does not exist');
+    test.todo('Should handle database errors gracefully');
+  });
+
+  describe('archive', () => {
+    test.todo('Should archive a DOT successfully');
+    test.todo('Should handle database errors gracefully');
+    test.todo('Should return a 404 error if the specified UUID does not exist');
+    test.todo('Throw ConflictException if the DOT is already archived');
+  });
+
+  describe('unarchive', () => {
+    test.todo('Should unarchive a DOT successfully');
+    test.todo('Should handle database errors gracefully');
+    test.todo('Should return a 404 error if the specified UUID does not exist');
+    test.todo('Throw ConflictException if the DOT is not archived');
+  });
 });
