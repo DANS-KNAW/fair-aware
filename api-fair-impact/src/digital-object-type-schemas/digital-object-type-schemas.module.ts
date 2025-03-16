df914531c4f251842875d93296fae7fb86dae0d3
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-03-16 11:21:26 +0100

    Removed circulaire imports

--- api-fair-impact/src/digital-object-type-schemas/digital-object-type-schemas.module.ts
+++ api-fair-impact/src/digital-object-type-schemas/digital-object-type-schemas.module.ts
@@ -1,27 +1,16 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { DigitalObjectTypeSchemasService } from './digital-object-type-schemas.service';
 import { DigitalObjectTypeSchemasController } from './digital-object-type-schemas.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DigitalObjectTypeSchema } from './entities/digital-object-type-schema.entity';
-import { ContentLanguageModulesModule } from 'src/content-language-modules/content-language-modules.module';
 import { LanguagesModule } from 'src/languages/languages.module';
-import { DigitalObjectTypesModule } from 'src/digital-object-types/digital-object-types.module';
-import { SchemasServiceFactory } from './schemas/schemas.service.factory';
-import { FAIRSchema } from './schemas/fair-schema.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([DigitalObjectTypeSchema]),
     LanguagesModule,
-    forwardRef(() => DigitalObjectTypesModule),
-    forwardRef(() => ContentLanguageModulesModule),
   ],
   controllers: [DigitalObjectTypeSchemasController],
-  providers: [
-    DigitalObjectTypeSchemasService,
-    SchemasServiceFactory,
-    FAIRSchema,
-  ],
-  exports: [DigitalObjectTypeSchemasService, SchemasServiceFactory],
+  providers: [DigitalObjectTypeSchemasService],
 })
 export class DigitalObjectTypeSchemasModule {}
