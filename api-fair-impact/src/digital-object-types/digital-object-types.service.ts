96758068a7909919659ef943feaf63e10ca807c4
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-03-16 13:36:35 +0100

    Implemented DOT creation

--- api-fair-impact/src/digital-object-types/digital-object-types.service.ts
+++ api-fair-impact/src/digital-object-types/digital-object-types.service.ts
@@ -1,4 +1,13 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { CreateDigitalObjectTypeDto } from './dto/create-digital-object-type.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DigitalObjectType } from './entities/digital-object-type.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class DigitalObjectTypesService {
@@ -6,9 +15,29 @@ export class DigitalObjectTypesService {
     timestamp: true,
   });
 
-  constructor() {}
+  constructor(
+    @InjectRepository(DigitalObjectType)
+    private readonly digitalObjectTypeRepository: Repository<DigitalObjectType>,
+  ) {}
 
-  async create() {}
+  async create(createDigtialObjectTypeDto: CreateDigitalObjectTypeDto) {
+    try {
+      let digitalObjectType = this.digitalObjectTypeRepository.create(
+        createDigtialObjectTypeDto,
+      );
+
+      digitalObjectType =
+        await this.digitalObjectTypeRepository.save(digitalObjectType);
+
+      return digitalObjectType;
+    } catch (error) {
+      this.logger.error(error);
+      if (error.code === '23505') {
+        throw new ConflictException('DOT code already exists!');
+      }
+      throw new InternalServerErrorException('Failed create DOT!');
+    }
+  }
 
   async findAll() {}
 
