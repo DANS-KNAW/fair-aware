a15659f23e21f4221b16addc4e47ec6bbbc1bc57
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-03-17 07:17:59 +0100

    Add controller for CLM

--- api-fair-impact/src/content-language-modules/content-language-modules.controller.ts
+++ api-fair-impact/src/content-language-modules/content-language-modules.controller.ts
@@ -1,5 +1,19 @@
-import { Controller, Get, Post, Patch, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Query,
+  ParseIntPipe,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { ContentLanguageModulesService } from './content-language-modules.service';
+import { CreateContentLanguageModuleDto } from './dto/create-content-language-module.dto';
+import { ParseISO639Pipe } from 'src/pipes/iso-639.pipe';
+import { UpdateContentLanguageModuleDto } from './dto/update-content-language-module.dto';
 
 @Controller('content-language-modules')
 export class ContentLanguageModulesController {
@@ -8,20 +22,48 @@ export class ContentLanguageModulesController {
   ) {}
 
   @Post()
-  create() {}
+  create(
+    @Body() createContentLanguageModuleDto: CreateContentLanguageModuleDto,
+  ) {
+    return this.contentLanguageModulesService.create(
+      createContentLanguageModuleDto,
+    );
+  }
 
   @Get()
-  findAll() {}
+  findAll(@Query('page', ParseIntPipe) page: number) {
+    return this.contentLanguageModulesService.findAll(page);
+  }
 
-  @Get()
-  findOne() {}
+  @Get('uuid')
+  findOne(@Param('uuid', new ParseUUIDPipe()) uuid: string) {
+    return this.contentLanguageModulesService.findOne(uuid);
+  }
 
-  @Get()
-  findByLanguageAndDot() {}
+  @Get('language/:language/dot/:dot')
+  findByLanguageAndDot(
+    @Param('language', new ParseISO639Pipe()) language: string,
+    @Param('dot') dot: string,
+  ) {
+    return this.contentLanguageModulesService.findByLanguageAndDot(
+      language,
+      dot,
+    );
+  }
 
-  @Patch()
-  update() {}
+  @Patch(':uuid')
+  update(
+    @Param('uuid', new ParseUUIDPipe()) uuid: string,
+    @Body() updateContentLanguageModuleDto: UpdateContentLanguageModuleDto,
+  ) {
+    return this.contentLanguageModulesService.update(
+      uuid,
+      updateContentLanguageModuleDto,
+    );
+  }
 
-  @Delete()
-  remove() {}
+  @Delete(':uuid')
+  remove(@Param('uuid', new ParseUUIDPipe()) id: string) {
+    return this.contentLanguageModulesService.remove(id);
+  }
 }
