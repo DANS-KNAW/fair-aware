45ab7ac0c4b3ac018652cecab1049276eace6d4a
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-03-17 07:21:35 +0100

    Add Language FindALl method

--- api-fair-impact/src/languages/languages.service.ts
+++ api-fair-impact/src/languages/languages.service.ts
@@ -1,10 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { Language } from './entities/language.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class LanguagesService {
-  constructor() {}
+  private readonly logger = new Logger(LanguagesService.name, {
+    timestamp: true,
+  });
 
-  async findAll() {}
+  constructor(
+    @InjectRepository(Language)
+    private readonly languageRepository: Repository<Language>,
+  ) {}
+
+  /**
+   * Retrieves all languages.
+   * @returns A list of languages.
+   */
+  async findAll(): Promise<Language[]> {
+    try {
+      // Returning it directly since at worst it will return an empty array.
+      // We don't need pagination as it is an finite list.
+      return this.languageRepository.find();
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException('Failed to fetch languages!');
+    }
+  }
 
   async findOne() {}
 
