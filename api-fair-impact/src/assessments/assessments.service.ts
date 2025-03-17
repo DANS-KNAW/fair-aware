dbb5ae86ce6593b4105fa1bc7bd793752ec4809d
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-03-17 08:20:06 +0100

    Add create method for assessments

--- api-fair-impact/src/assessments/assessments.service.ts
+++ api-fair-impact/src/assessments/assessments.service.ts
@@ -1,4 +1,15 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { Assessment } from './entities/assessment.entity';
+import { CreateAssessmentDto } from './dto/create-assessment.dto';
+import { LanguagesService } from 'src/languages/languages.service';
+import { DigitalObjectTypesService } from 'src/digital-object-types/digital-object-types.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AssessmentsService {
@@ -6,10 +17,37 @@ export class AssessmentsService {
     timestamp: true,
   });
 
-  constructor() {}
+  constructor(
+    @InjectRepository(Assessment)
+    private readonly assessmentRepository: Repository<Assessment>,
+    private readonly languagesService: LanguagesService,
+    private readonly digitalObjectTypesService: DigitalObjectTypesService,
+  ) {}
 
-  async create() {}
+  /**
+   * Creates a new assessment.
+   * @param createAssessmentDto - The data to create the assessment.
+   * @returns The created assessment.
+   */
+  async create(createAssessmentDto: CreateAssessmentDto): Promise<Assessment> {
+    try {
+      // Check if the language and digital object type exist.
+      // @todo should update entity for actual relation.
+      await this.languagesService.findOne(createAssessmentDto.languageCode);
+      await this.digitalObjectTypesService.findOne(createAssessmentDto.dotCode);
 
+      let assessment = this.assessmentRepository.create(createAssessmentDto);
+      assessment = await this.assessmentRepository.save(assessment);
+
+      return assessment;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      this.logger.error(error);
+      throw new InternalServerErrorException('Failed to create assessment!');
+    }
+  }
   async findAll() {}
 
   async findOne() {}
