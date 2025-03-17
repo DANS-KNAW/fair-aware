99815318588b8c75a7bfb8e2a181335ac673584e
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-03-17 07:00:11 +0100

    Add create method of CLM

--- api-fair-impact/src/content-language-modules/content-language-modules.service.ts
+++ api-fair-impact/src/content-language-modules/content-language-modules.service.ts
@@ -1,4 +1,13 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ContentLanguageModule } from './entities/content-language-module.entity';
+import { Repository } from 'typeorm';
+import { CreateContentLanguageModuleDto } from './dto/create-content-language-module.dto';
 
 @Injectable()
 export class ContentLanguageModulesService {
@@ -6,9 +15,40 @@ export class ContentLanguageModulesService {
     timestamp: true,
   });
 
-  constructor() {}
+  constructor(
+    @InjectRepository(ContentLanguageModule)
+    private contentLanguageModuleRepository: Repository<ContentLanguageModule>,
+  ) {}
 
-  async create() {}
+  /**
+   * Creates a new content language module.
+   * @param createContentLanguageModuleDto - The data to create the new content language module.
+   * @returns The created content language module.
+   */
+  async create(
+    createContentLanguageModuleDto: CreateContentLanguageModuleDto,
+  ): Promise<ContentLanguageModule> {
+    try {
+      let contentLanguageModule = this.contentLanguageModuleRepository.create(
+        createContentLanguageModuleDto,
+      );
+
+      contentLanguageModule = await this.contentLanguageModuleRepository.save(
+        contentLanguageModule,
+      );
+
+      return contentLanguageModule;
+    } catch (error) {
+      this.logger.error(error);
+      // Check if the error is a unique constraint violation.
+      if (error.code === '23505') {
+        throw new ConflictException('Content language module already exists!');
+      }
+      throw new InternalServerErrorException(
+        'Failed to create content language module!',
+      );
+    }
+  }
 
   async findAll() {}
 
