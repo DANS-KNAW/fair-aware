f9b405fe7f617b1bcb34e530656a543cdcb53dab
paulboon <paul.boon@dans.knaw.nl>
2025-04-02 11:34:11 +0200

    Commented out social media links in footer

--- client-fair-impact/components/footer/footer.tsx
+++ client-fair-impact/components/footer/footer.tsx
@@ -84,11 +84,11 @@ export default function Footer() {
           </div>
         </div>
         <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
-          <div className="flex space-x-6 md:order-2">
+          {/* <div className="flex space-x-6 md:order-2">
             <LinkedinIcon />
             <TwitterIcon />
             <WatchappIcon />
-          </div>
+          </div> */}
           <p className="mt-8 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
             &copy; Copyright 2024 - FAIRsFAIR
           </p>
