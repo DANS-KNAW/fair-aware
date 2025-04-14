736a2df6418620e354a7de8616f7d8e3b962b98c
Paul Boon <paul.boon@dans.knaw.nl>
2025-04-14 11:43:43 +0200

    Fr 17 fix glossary create api (#26)
    * Create glossary via API, but without items
    
    * Added logging
    
    * Added glossary deletion via the API
    
    * Fixed glossary create and delete
    
    * Using upsert in glossary seeding and added unique constraint on Glossary
    
    * Adde delete for glossary by language and dot
    
    * Formatting
    
    * Seeding of glossary is with save, because upsert did not add the items

--- api-fair-impact/src/glossaries/dto/create-glossery.dto.ts
+++ api-fair-impact/src/glossaries/dto/create-glossery.dto.ts
@@ -1,9 +1,34 @@
-import { PickType } from '@nestjs/mapped-types';
-import { Glossary } from '../entities/glossary.entity';
+import { IsGlobalAlpha } from 'src/decorators/is-global-alpha';
+import { CreateGlossaryItemDto } from './create-glossary-item.dto';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  MaxLength,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { GlossaryItem } from '../entities/glossary-item.entity';
 
-export class CreateGlossaryDto extends PickType(Glossary, [
-  'title',
-  'items',
-  'language',
-  'digitalObjectType',
-] as const) {}
+export class CreateGlossaryDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  languageCode: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsGlobalAlpha()
+  @MaxLength(6)
+  digitalObjectTypeCode: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateGlossaryItemDto)
+  items: CreateGlossaryItemDto[];
+  //items: GlossaryItem[];
+}
--- api-fair-impact/src/glossaries/glossaries.service.ts
+++ api-fair-impact/src/glossaries/glossaries.service.ts
@@ -23,12 +23,64 @@ export class GlossariesService {
   ) {}
 
   async create(createGlossaryDto: CreateGlossaryDto): Promise<Glossary> {
-    let glossary = this.glossaryRepository.create({
-      ...createGlossaryDto,
+    // get the language and digital object type from the database
+    const { languageCode, digitalObjectTypeCode, items, ...rest } =
+      createGlossaryDto;
+    const language = await this.glossaryRepository.manager
+      .getRepository('Language')
+      .findOneBy({ code: languageCode });
+    const digitalObjectType = await this.glossaryRepository.manager
+      .getRepository('DigitalObjectType')
+      .findOneBy({ code: digitalObjectTypeCode });
+    if (!language) {
+      throw new NotFoundException(
+        `Language with code ${languageCode} not found!`,
+      );
+    }
+    if (!digitalObjectType) {
+      throw new NotFoundException(
+        `Digital Object Type with code ${digitalObjectTypeCode} not found!`,
+      );
+    }
+    this.logger.debug('Rest: ' + JSON.stringify(rest, null, 2));
+
+    this.logger.debug('Items: ' + JSON.stringify(items, null, 2));
+
+    const glossary = this.glossaryRepository.create({
+      ...rest,
+      language,
+      digitalObjectType,
+      items,
     });
+    this.logger.debug(
+      `Creating glossary with title: ${glossary.title}, language: ${language.code}, and digital object type: ${digitalObjectType.code}`,
+    );
+
+    this.logger.debug('Glossary created: ' + JSON.stringify(glossary, null, 2));
 
     try {
-      glossary = await this.glossaryRepository.save(glossary);
+      // find the glossary by language and digital object type
+      const existingGlossary = await this.glossaryRepository.findOne({
+        where: {
+          language: { code: language.code },
+          digitalObjectType: { code: digitalObjectType.code },
+        },
+        relations: {
+          digitalObjectType: true,
+          language: true,
+          //items: true,
+        },
+      });
+
+      if (existingGlossary) {
+        // if it exists save gives an error (language and digital object type are not unique)
+        // but upsert does not work... it does not change the existing glossary items
+        throw new InternalServerErrorException(
+          `Glossary with language ${language.code} and digital object type code ${digitalObjectType.code} already exists!`,
+        );
+      }
+
+      await this.glossaryRepository.save(glossary);
     } catch (error) {
       this.logger.error(error);
       throw new InternalServerErrorException('Failed to create Glossary!');
@@ -122,6 +174,52 @@ export class GlossariesService {
     } catch (error) {
       this.logger.error(error);
       throw new InternalServerErrorException('Failed to find Glossary!');
+    }
+  }
+
+  async removeByLanguageAndDot(
+    language: string,
+    digitalObjectTypeCode: string,
+  ): Promise<void> {
+    try {
+      const glossary = await this.findByLanguageAndDot(
+        language,
+        digitalObjectTypeCode,
+      );
+      if (!glossary) {
+        throw new NotFoundException(
+          `Glossary with language ${language} and digital object type code ${digitalObjectTypeCode} not found!`,
+        );
+      }
+      return await this.remove(glossary.uuid);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      this.logger.error(error);
+      throw new InternalServerErrorException(
+        `Failed to remove glossary by language: ${language} and digital object type code: ${digitalObjectTypeCode}!`,
+      );
+    }
+  }
+
+  async remove(uuid: string): Promise<void> {
+    try {
+      const glossary = await this.glossaryRepository.findOne({
+        where: { uuid },
+      });
+      if (!glossary) {
+        throw new NotFoundException(`Glossary with uuid ${uuid} not found!`);
+      }
+      // remove all its items
+      await this.glossaryRepository.manager
+        .getRepository('GlossaryItem')
+        .delete({ glossary: { uuid } });
+      // remove the glossary;
+      await this.glossaryRepository.remove(glossary);
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException('Failed to remove Glossary!');
     }
   }
 }
