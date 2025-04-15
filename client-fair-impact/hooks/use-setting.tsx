19a6748c2ee73c07f4ef2120b6b36bba0be89b37
paulboon <paul.boon@dans.knaw.nl>
2025-04-15 09:26:33 +0200

    improving the use-settings hook

--- client-fair-impact/hooks/use-setting.tsx
+++ client-fair-impact/hooks/use-setting.tsx
@@ -4,11 +4,13 @@ import { useQuery } from "@tanstack/react-query";
 export const fetchSetting = async (
     id: string,
   ): Promise<ISetting> => {
+    console.log(`fetchSetting on: ${process.env.NEXT_PUBLIC_API_HOST}/settings/${id}`);
+    
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_HOST}/settings/${id}`,
     );
     if (!response.ok) {
-      throw new Error(`Failed to retrieve setting : ${id}`);
+      throw new Error(`Failed to retrieve setting: ${id}`);
     }
 
     // TODO: possibly handle specific settings here, with validation and defaults
@@ -16,7 +18,7 @@ export const fetchSetting = async (
     return response.json();
   };
   
-  export default function useGlossary(id: string, dependencyEnabled?: boolean) {
+  export default function useSetting(id: string, dependencyEnabled?: boolean) {
     return useQuery<ISetting>({
       queryKey: ["setting", id],
       queryFn: () => fetchSetting(id),
