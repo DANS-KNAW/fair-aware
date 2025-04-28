ce8198ee6739ab562b3fae5f56592e2f71e21598
paulboon <paul.boon@dans.knaw.nl>
2025-04-28 10:21:56 +0200

    Use prebuild script to generate version

--- client-fair-impact/hooks/get-version.tsx
+++ client-fair-impact/hooks/get-version.tsx
@@ -1,9 +1,6 @@
-//import { VERSION } from '../version';
+import { VERSION } from '../version';
 
 export default function useAppVersion() {
-    // Could get from the API, if needed
-    // this is from the env
-    //return `${process.env.NEXT_PUBLIC_APP_VERSION}` || 'dev';
-    //return  VERSION || ''; // from prebuild, using the package.json version and a prebuild script
-    return "0.0.0"; // hardcoded version, not from env or package.json
+    // Could get from the API  or from an env, if needed
+    return  VERSION || ''; // from prebuild, using the package.json version and a prebuild script
 }
