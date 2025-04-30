79bc4a1397f84bd172bdc3b72425976e027e1564
paulboon <paul.boon@dans.knaw.nl>
2025-04-30 09:58:26 +0200

    Fix DOT delete with glossary

--- api-fair-impact/src/digital-object-types/digital-object-types.module.ts
+++ api-fair-impact/src/digital-object-types/digital-object-types.module.ts
@@ -4,11 +4,13 @@ import { DigitalObjectTypesController } from './digital-object-types.controller'
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DigitalObjectType } from './entities/digital-object-type.entity';
 import { DigitalObjectTypeSchemasModule } from 'src/digital-object-type-schemas/digital-object-type-schemas.module';
+import { GlossariesModule } from 'src/glossaries/glossaries.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([DigitalObjectType]),
     forwardRef(() => DigitalObjectTypeSchemasModule),
+    forwardRef(() => GlossariesModule),
   ],
   controllers: [DigitalObjectTypesController],
   providers: [DigitalObjectTypesService],
