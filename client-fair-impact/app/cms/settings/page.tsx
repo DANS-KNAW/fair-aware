c21e5cc921ff4b50a7367496e2c9e3cdd7e3be0c
paulboon <paul.boon@dans.knaw.nl>
2025-05-13 10:24:39 +0200

    Added minimal settings page

---
+++ client-fair-impact/app/cms/settings/page.tsx
@@ -0,0 +1,15 @@
+export default async function SettingsCMSPage() {
+    return (
+        <>
+        <div className="flex flex-col items-center justify-between sm:flex-row">
+            <div>
+            <h1 className="mb-2 text-2xl font-bold text-gray-800">Settings</h1>
+            <p className="text-base text-gray-600">
+                Manage your settings and preferences here.
+            </p>
+            </div>
+        </div>
+        </>
+    );
+}   
+
