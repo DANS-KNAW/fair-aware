5e0a378260a47a4db32fa5d884e7d2d27a66411e
paulboon <paul.boon@dans.knaw.nl>
2025-05-14 08:53:12 +0200

    Initial attempt for editing of the settings

--- client-fair-impact/app/cms/settings/client.tsx
+++ client-fair-impact/app/cms/settings/client.tsx
@@ -1,9 +1,205 @@
 "use client";
 
 import Breadcrumbs from "@/components/beardcrumbs";
+import BasicTextInput from "@/components/form/basic-text-input";
+import Editor from "@/components/form/lexical/editor";
+import UrlInput from "@/components/form/url-input";
+import { ToastContext } from "@/context/toast-context";
 import useSetting from "@/hooks/use-setting";
+import PatchSettingFetch from "@/lib/mutations/patch-setting-fetch";
+import { getQueryClient } from "@/lib/query-provider";
+import { ISetting } from "@/types/entities/setting.interface";
+import { IFormCreateSettings } from "@/types/form/form-create-settings";
+import { useMutation } from "@tanstack/react-query";
+import { useContext, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+
+function ReadView({
+  contactEmail,
+  privacyPolicyLink,
+  introductionText,
+}: {
+  contactEmail: ISetting | undefined;
+  privacyPolicyLink: ISetting | undefined;
+  introductionText: ISetting | undefined;
+}) {
+  return (
+    <>
+      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 border-t border-gray-300 py-6 sm:grid-cols-6">
+        <div className="sm:col-span-6">
+          <label className="block text-sm/6 font-medium text-gray-900">
+            Contact Email
+          </label>
+          <div className="mt-2">
+            <p className="block min-h-[2.375rem] w-full rounded-md border border-gray-300 bg-gray-400/5 px-3 py-1.5 text-base text-gray-900 sm:text-sm/6">
+              {contactEmail?.value || "No contact email set."}
+            </p>
+          </div>
+        </div>
+        <div className="sm:col-span-6">
+          <label className="block text-sm/6 font-medium text-gray-900">
+            Privacy Policy
+          </label>
+          <div className="mt-2">
+            <p className="block min-h-[2.375rem] w-full rounded-md border border-gray-300 bg-gray-400/5 px-3 py-1.5 text-base text-gray-900 sm:text-sm/6">
+              {privacyPolicyLink?.value || "No privacy policy link set."}
+            </p>
+          </div>
+        </div>
+        <div className="sm:col-span-6">
+          <label className="block text-sm/6 font-medium text-gray-900">
+            Introduction Text
+          </label>
+          <div
+            className="prose prose-a:text-fair_dark_blue-600 prose-a:hover:text-fair_dark_blue-500 block min-h-[2.375rem] w-full min-w-full rounded-md border border-gray-300 bg-gray-400/5 px-3 py-1.5 text-base text-gray-900 sm:text-sm/6"
+            dangerouslySetInnerHTML={{ __html: introductionText?.value || "" }}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function EditView({
+  contactEmail,
+  privacyPolicyLink,
+  introductionText,
+  handleEditMode,
+}: {
+  contactEmail: ISetting | undefined;
+  privacyPolicyLink: ISetting | undefined;
+  introductionText: ISetting | undefined;
+  handleEditMode: () => void;
+}) {
+  const queryClient = getQueryClient();
+  const { register, handleSubmit, control } = useForm<IFormCreateSettings>({
+    defaultValues: {
+      contactEmail: { id: "ContactEmail", value: contactEmail?.value },
+      privacyPolicyLink: {
+        id: "PrivacyPolicyLink",
+        value: privacyPolicyLink?.value,
+      },
+      introductionText: {
+        id: "IntroductionText",
+        value: introductionText?.value,
+      },
+    },
+  });
+  const toasts = useContext(ToastContext);
+
+  const mutation = useMutation({
+    mutationFn: async (newSettings: IFormCreateSettings) => {
+      // log the new settings
+      console.log("In mutationFn: New settings: ", newSettings);
+      // patch the settings, just all of them
+      PatchSettingFetch(newSettings.contactEmail);
+      PatchSettingFetch(newSettings.privacyPolicyLink);
+      PatchSettingFetch(newSettings.introductionText);
+    },
+    onSuccess: () => {
+      toasts.setToasts({
+        type: "success",
+        message: "Successfully updated!",
+        subtext: "Glossary has been updated successfully.",
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["contactEmail"],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["privacyPolicyLink"],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["introductionText"],
+      });
+      handleEditMode();
+    },
+    onError: (error) => {
+      toasts.setToasts({
+        type: "error",
+        message: "Failed to update Glossary.",
+        subtext: `Error: ${error instanceof Error ? error.message : "Unknown error"}`,
+      });
+    },
+  });
+
+  return (
+    <>
+      <form
+        id="SETTINGS-EDIT-FORM"
+        onSubmit={handleSubmit((data) => {
+          // construct the data to be sent to the server
+        //   const newSettings: IFormCreateSettings = {
+        //     contactEmail: {
+        //       id: "ContactEmail",
+        //       value: data.contactEmail.value,
+        //     },
+        //     privacyPolicyLink: {
+        //       id: "PrivacyPolicyLink",
+        //       value: data.privacyPolicyLink.value,
+        //     },
+        //     introductionText: {
+        //       id: "IntroductionText",
+        //       value: data.introductionText.value,
+        //     },
+        //   };
+          const newSettings: IFormCreateSettings = data;
+          // log the new settings
+          console.log("In onSubmit: New settings: ", newSettings);
+          mutation.mutate(newSettings);
+        })}
+      >
+        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 border-t border-gray-300 py-6 sm:grid-cols-6">
+          <div className="sm:col-span-6">
+            <label className="block text-sm/6 font-medium text-gray-900">
+              Contact Email
+            </label>
+            <div className="mt-2">
+                   <BasicTextInput
+                     register={register}
+                     name={`contactEmail.value`}
+                   />
+            </div>
+          </div>
+          <div className="sm:col-span-6">
+            <label className="block text-sm/6 font-medium text-gray-900">
+              Privacy Policy
+            </label>
+            <div className="mt-2">
+              <UrlInput register={register} name={`privacyPolicyLink.value`} />
+            </div>
+          </div>
+          <div className="sm:col-span-6">
+            <label className="block text-sm/6 font-medium text-gray-900">
+              Introduction Text
+            </label>
+            <div className="mt-2">
+              <Controller
+                name={`introductionText.value`}
+                control={control}
+                defaultValue=""
+                //rules={{ required: "This field is required" }}
+                render={({ field, fieldState: { error } }) => (
+                  <>
+                    <Editor
+                      namespace={`introductionText.value`}
+                      value={field.value}
+                      onChange={field.onChange}
+                    />
+                    {error && <p className="error">{error.message}</p>}
+                  </>
+                )}
+              />
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+}
 
 export default function SettingsClient() {
+  const [editMode, setEditMode] = useState(false);
+
   // The settings, maybe more efficient to fetch them all at once?
   const {
     data: contactEmail,
@@ -37,6 +233,10 @@ export default function SettingsClient() {
     return <h1 className="text-2xl font-bold text-gray-800">Error</h1>;
   }
 
+  const handleEditMode = () => {
+    setEditMode((editMode) => !editMode);
+  };
+
   return (
     <>
       <Breadcrumbs />
@@ -47,38 +247,46 @@ export default function SettingsClient() {
             Manage your settings and preferences here.
           </p>
         </div>
-      </div>
-      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 border-t border-gray-300 py-6 sm:grid-cols-6">
-        <div className="sm:col-span-6">
-          <label className="block text-sm/6 font-medium text-gray-900">
-            Contact Email
-          </label>
-          <div className="mt-2">
-            <p className="block min-h-[2.375rem] w-full rounded-md border border-gray-300 bg-gray-400/5 px-3 py-1.5 text-base text-gray-900 sm:text-sm/6">
-              {contactEmail?.value || "No contact email set."}
-            </p>
+        {editMode ? (
+          <div className="flex space-x-8">
+            <button
+              type="submit"
+              form="SETTINGS-EDIT-FORM"
+              className="bg-fair_dark_blue-600 hover:bg-fair_dark_blue-500 focus-visible:outline-fair_dark_blue-600 flex cursor-pointer items-center rounded-md px-3 py-2 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2"
+            >
+              Save
+            </button>
+            <button
+              type="button"
+              onClick={handleEditMode}
+              className="cursor-pointer text-sm/6 font-semibold text-gray-900"
+            >
+              Cancel
+            </button>
           </div>
-        </div>
-        <div className="sm:col-span-6">
-          <label className="block text-sm/6 font-medium text-gray-900">
-            Privacy Policy
-          </label>
-          <div className="mt-2">
-            <p className="block min-h-[2.375rem] w-full rounded-md border border-gray-300 bg-gray-400/5 px-3 py-1.5 text-base text-gray-900 sm:text-sm/6">
-              {privacyPolicyLink?.value || "No privacy policy link set."}
-            </p>
-          </div>
-        </div>
-        <div className="sm:col-span-6">
-          <label className="block text-sm/6 font-medium text-gray-900">
-            Introduction Text
-          </label>
-          <div
-            className="prose prose-a:text-fair_dark_blue-600 prose-a:hover:text-fair_dark_blue-500 block min-h-[2.375rem] w-full min-w-full rounded-md border border-gray-300 bg-gray-400/5 px-3 py-1.5 text-base text-gray-900 sm:text-sm/6"
-            dangerouslySetInnerHTML={{ __html: introductionText?.value || "" }}
-          />
-        </div>
+        ) : (
+          <button
+            onClick={handleEditMode}
+            className="bg-fair_dark_blue-600 hover:bg-fair_dark_blue-500 focus:ring-fair_dark_blue-500 mt-4 rounded-md px-4 py-2 text-sm font-medium text-white shadow-sm focus:ring-2 focus:ring-offset-2 focus:outline-none"
+          >
+            Edit
+          </button>
+        )}
       </div>
+      {editMode ? (
+        <EditView
+          contactEmail={contactEmail}
+          privacyPolicyLink={privacyPolicyLink}
+          introductionText={introductionText}
+          handleEditMode={handleEditMode}
+        />
+      ) : (
+        <ReadView
+          contactEmail={contactEmail}
+          privacyPolicyLink={privacyPolicyLink}
+          introductionText={introductionText}
+        />
+      )}
     </>
   );
 }
